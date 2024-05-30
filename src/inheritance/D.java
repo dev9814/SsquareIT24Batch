f6cd733bda964f85f339c8c8641c165f5d5e6ee4
dev9814 <devdutt.software@gmail.com>
2024-05-30 19:43:15 +0530

    my sixth commit - validate

---
+++ src/inheritance/D.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class D {
+
+	public static void main(String[] args) {
+		int i=10;
+		
+		System.out.println(i);
+
+	}
+
+}
---
+++ src/test_cases/C.java
@@ -0,0 +1,11 @@
+package test_cases;
+
+public class C {
+
+	public static void main(String[] args) {
+		
+		System.out.println("class c");
+
+	}
+
+}
